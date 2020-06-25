d3fbf33ab5b8d24a61083a55d32bda01029f6216
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-06-25 11:26:29 -0400

    wrote autoMoveCars and moveCarLeft

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const timeLeft = document.querySelector('#time-left')
     const result = document.querySelector('#result')
     const startBtn = document.querySelector('#button')
+    const carsLeft = document.querySelector('.car-left')
+    const carsRight = document.querySelector('.car-right')
+    const logsLeft = document.querySelector('.log-left')
+    const logsRight = document.querySelector('.log-right')
 
     const width = 9
     let currentIndex = 76
@@ -32,5 +36,31 @@ document.addEventListener('DOMContentLoaded', () => {
         win()
     }
 
+    //move cars
+    function autoMoveCars() {
+        carsLeft.forEach(carLeft => moveCarLeft(carLeft))
+        carsRight.forEach(carRight => moveCarRight(carRight))
+    }
+
+    //move the car left in a time loop
+    function moveCarLeft(carLeft) {
+        switch (true) {
+            case carLeft.classList.contains('c1'):
+                carLeft.classList.remove('c1')
+                carLeft.classList.add('c2')
+                break
+            case carLeft.classList.contains('c2'):
+                carLeft.classList.remove('c2')
+                carLeft.classList.add('c3')
+                break
+            case carLeft.classList.contains('c3'):
+                carLeft.classList.remove('c3')
+                carLeft.classList.add('c1')
+                break
+
+        }
+    }
+
+    
 
 })
