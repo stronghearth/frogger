c7b00595f7af7e9e2be3d5a50e4614c094ccf450
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-06-25 11:43:50 -0400

    wrote functions for moving logs and moving cars

--- app.js
+++ app.js
@@ -61,6 +61,85 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    
+    //move the car right in a time loop
+    function moveCarRight(carRight) {
+        switch (true) {
+            case carRight.classList.contains('c1'):
+                carRight.classList.remove('c1')
+                carRight.classList.add('c3')
+                break
+            case carRight.classList.contains('c2'):
+                carRight.classList.remove('c2')
+                carRight.classList.add('c1')
+                break
+            case carRight.classList.contains('c3'):
+                carRight.classList.remove('c3')
+                carRight.classList.add('c2')
+                break
 
+        }
+    }
+
+    //move the logs
+    function autoMoveLogs() {
+        logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+        logsRight.forEach(logRight => moveLogRight(logRight))
+    }
+
+    //move the log left in a time loop
+    function moveLogLeft(logLeft) {
+        switch (true) {
+            case logLeft.classList.contains('l1'):
+                logLeft.classList.remove('l1')
+                logLeft.classList.add('l2')
+                break
+            case logLeft.classList.contains('l2'):
+                logLeft.classList.remove('l2')
+                logLeft.classList.add('l3')
+                break
+            case logLeft.classList.contains('l3'):
+                logLeft.classList.remove('l3')
+                logLeft.classList.add('l4')
+                break
+            case logLeft.classList.contains('l4'):
+                logLeft.classList.remove('l4')
+                logLeft.classList.add('l5')
+                break
+            case logLeft.classList.contains('l5'):
+                logLeft.classList.remove('l5')
+                logLeft.classList.add('l1')
+                break
+        }
+    }
+
+    //move the log right in a time loop
+    function moveLogRight(logRight) {
+        switch (true) {
+            case logRight.classList.contains('l1'):
+                logRight.classList.remove('l1')
+                logRight.classList.add('l5')
+                break
+            case logRight.classList.contains('l2'):
+                logRight.classList.remove('l2')
+                logRight.classList.add('l1')
+                break
+            case logRight.classList.contains('l3'):
+                logRight.classList.remove('l3')
+                logRight.classList.add('l2')
+                break
+            case logLeft.classList.contains('l4'):
+                logLeft.classList.remove('l4')
+                logLeft.classList.add('l3')
+                break
+            case logLeft.classList.contains('l5'):
+                logLeft.classList.remove('l5')
+                logLeft.classList.add('l4')
+                break
+        }
+    }
+
+    //rules to win frogger
+    function win() {
+        if(squares)
+    }
 })
