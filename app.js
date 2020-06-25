3cf0de53585a01d808663606c34ca7332207eba0
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-06-25 10:49:14 -0400

    wrote basic moveFrog function

---
+++ app.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const timeLeft = document.querySelector('#time-left')
+    const result = document.querySelector('#result')
+    const startBtn = document.querySelector('#button')
+
+    const width = 9
+    let currentIndex = 76
+    let timerId
+
+    squares[currentIndex].classList.add('frog')
+
+    //function that allows the index with the frog class to change with certain keystrokes
+    function moveFrog(e) {
+        squares[currentIndex].classList.remove('frog')
+        switch(e.keyCode) {
+            case 37:
+                if(currentIndex % width !== 0) currentIndex -= 1
+                break
+            case 38:
+                if(currentIndex - width >= 0) currentIndex -= width
+                break
+            case 39:
+                if(currentIndex % width < width - 1) currentIndex += 1
+                break
+            case 40:
+                if(currentIndex + width < width * width) currentIndex += width
+                break
+        }
+        squares[currentIndex].classList.add('frog')
+        lose()
+        win()
+    }
+
+
+})
